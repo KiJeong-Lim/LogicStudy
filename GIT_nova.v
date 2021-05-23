14e899778bfb3594f599227d99ceed64e7197620
KiJeong-Lim <gijungdduk@naver.com>
2021-05-23 10:14:02 +0900

    .

--- GIT_nova.v
+++ GIT_nova.v
@@ -298,122 +298,6 @@ Proof with eauto.
   - simpl...
 Qed.
 
-Inductive Arith : Set :=
-| varArith : nat -> Arith
-| plusArith : Arith -> Arith -> Arith
-| multArith : Arith -> Arith -> Arith
-| ltArith : Arith -> Arith -> Arith
-| muArith : Arith -> Arith
-.
-
-Definition varEval (m : nat) (n : nat) : Arity (m + S n) w :=
-  assocArity w m (S n) (pureArity m (pureArity n))
-.
-
-Definition plusEval (n : nat) : Arity n w -> Arity n w -> Arity n w :=
-  fun val1 : Arity n w => fun val2 : Arity n w => apArity n (apArity n (pureArity n plus) val1) val2
-.
-
-Definition multEval (n : nat) : Arity n w -> Arity n w -> Arity n w :=
-  fun val1 : Arity n w => fun val2 : Arity n w => apArity n (apArity n (pureArity n mult) val1) val2
-.
-
-Definition ltEval (n : nat) : Arity n w -> Arity n w -> Arity n w :=
-  fun val1 : Arity n w => fun val2 : Arity n w => apArity n (apArity n (pureArity n (fun x : w => fun y : w => if Compare_dec.lt_dec x y then 0 else 1)) val1) val2
-.
-
-Definition muEval (n : nat) : Arity (S n) w -> Arity n w -> Arity n w :=
-  fun val1 : Arity (S n) w => fun witness : Arity n w => apArity n (apArity n (pureArity n first_nat) (apArity n (pureArity n (fun f : w -> w => fun x : w => Nat.eqb (f x) 0)) (shiftArity_left n val1))) witness
-.
-
-Inductive RuleArith : forall n : nat, Arith -> Arity n w -> Prop :=
-| varRule :
-  forall n : nat,
-  forall i : nat,
-  RuleArith (n + S i) (varArith i) (varEval n i)
-| plusRule :
-  forall n : nat,
-  forall e1 : Arith,
-  forall e2 : Arith,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  RuleArith n e1 val1 ->
-  RuleArith n e2 val2 ->
-  RuleArith n (plusArith e1 e2) (plusEval n val1 val2)
-| multRule :
-  forall n : nat,
-  forall e1 : Arith,
-  forall e2 : Arith,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  RuleArith n e1 val1 ->
-  RuleArith n e2 val2 ->
-  RuleArith n (multArith e1 e2) (multEval n val1 val2)
-| ltRule :
-  forall n : nat,
-  forall e1 : Arith,
-  forall e2 : Arith,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  RuleArith n e1 val1 ->
-  RuleArith n e2 val2 ->
-  RuleArith n (ltArith e1 e2) (ltEval n val1 val2)
-| muRule :
-  forall n : nat,
-  forall e1 : Arith,
-  forall val1 : Arity (S n) w,
-  forall witness : Arity n w,
-  universal n (apArity n (apArity n (pureArity n (fun f : w -> w => fun x : w => f x = 0)) (shiftArity_left n val1)) witness) ->
-  RuleArith (S n) e1 val1 ->
-  RuleArith n (muArith e1) (muEval n val1 witness)
-.
-
-Lemma upgradeRule_once :
-  forall n : nat,
-  forall e : Arith,
-  forall val : Arity n w,
-  RuleArith n e val ->
-  RuleArith (S n) e (pureArity 1 val).
-Proof with eauto.
-  intros n e val H; induction H.
-  - apply (varRule (S n))...
-  - apply (plusRule (S n))...
-  - apply (multRule (S n))...
-  - apply (ltRule (S n))...
-  - apply (muRule (S n))... simpl...
-Qed.
-
-Lemma upgradeRule (m : nat) :
-  forall n : nat,
-  forall e : Arith,
-  forall val : Arity n w,
-  RuleArith n e val ->
-  RuleArith (m + n) e (assocArity w m n (pureArity m val)).
-Proof with eauto.
-  induction m...
-  simpl; intros; apply upgradeRule_once...
-Qed.
-
-Ltac execRule :=
-  simpl; tryif (
-    match goal with
-    | |- RuleArith _ _ (varEval ?n ?i) => apply (varRule n i)
-    | |- RuleArith _ _ (plusEval ?n _ _) => apply (plusRule n)
-    | |- RuleArith _ _ (multEval ?n _ _) => apply (multRule n)
-    | |- RuleArith _ _ (ltEval ?n _ _) => apply (ltRule n)
-    | |- RuleArith _ _ (muEval ?n _ _) => apply (muRule n)
-    | _ => fail
-    end
-  ) then execRule else tryif (
-    tryif (
-      match goal with
-      | |- RuleArith _ _ (pureArity ?n _) => rewrite <- (Nat.add_0_r n); rewrite (pure_assoc_pure n)
-      | _ => fail
-      end
-    ) then eapply upgradeRule else (eapply upgradeRule_once || eapply upgradeRule)
-  ) then execRule else eauto
-.
-
 Definition extensionality (A : Type) (n : nat) : Arity n A -> Arity n A -> Prop :=
   fun val1 : Arity n A => fun val2 : Arity n A => universal n (liftArity2 n (fun x1 : A => fun x2 : A => x1 = x2) val1 val2)
 .
@@ -491,371 +375,70 @@ Proof with eauto.
   - simpl...
 Qed.
 
-Ltac Arity_eq := apply (existsT_snd_eq (fun x : nat => Arity x w)); eauto.
+Inductive arith : Set :=
+| projA : nat -> arith
+| plusA : arith
+| multA : arith
+| lessA : arith
+| liftA : arith -> arith
+| callA : arith -> arith -> arith
+| miniA : arith -> arith
+.
 
-Lemma Rule_unique :
-  forall e : Arith,
+Definition proj (n : nat) : Arity (S n) w :=
+  pureArity n
+.
+
+Definition less (x : w) (y : w) : w :=
+  if Compare_dec.lt_dec x y then 0 else 1
+.
+
+Definition lift (m : nat) (n : nat) (val1 : Arity n w) : Arity (m + n) w :=
+  assocArity w m n (pureArity m val1)
+.
+
+Definition call (n : nat) (val1 : Arity (S n) w) (val2 : Arity n w) : Arity n w :=
+  apArity n (shiftArity_left n val1) val2
+.
+
+Definition mini (n : nat) (val1 : Arity (S n) w) (witness : Arity n w) : Arity n w :=
+  liftArity2 n first_nat (liftArity1 n (fun f : w -> w => fun x : w => Nat.eqb (f x) 0) (shiftArity_left n val1)) witness
+.
+
+Inductive evalArith : forall n : nat, arith -> Arity n w -> Prop :=
+| projE :
   forall n : nat,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  RuleArith n e val1 ->
-  RuleArith n e val2 ->
-  extensionality w n val1 val2.
-Proof with eauto.
-  assert ( claim1 :
-    forall n : nat,
-    forall val1 : Arity (S n) w,
-    forall val2 : Arity (S n) w,
-    extensionality w (S n) val1 val2 ->
-    forall witness1 : Arity n w,
-    forall witness2 : Arity n w,
-    universal n (apArity n (apArity n (pureArity n (fun f : w -> w => fun x : w => f x = 0)) (shiftArity_left n val1)) witness1) ->
-    universal n (apArity n (apArity n (pureArity n (fun f : w -> w => fun x : w => f x = 0)) (shiftArity_left n val2)) witness2) ->
-    extensionality w n (muEval n val1 witness1) (muEval n val2 witness2)
-  ).
-  { unfold extensionality.
-    induction n.
-    - unfold muEval.
-      unfold liftArity2.
-      simpl...
-      intros val1 val2 ext_eq.
-      assert (forall m : w, first_nat (fun x : w => val1 x =? 0) m = first_nat (fun x : w => val2 x =? 0) m).
-      { induction m...
-        simpl.
-        rewrite ext_eq.
-        rewrite IHm...
-      }
-      intros.
-      rewrite H.
-      rewrite ext_eq in H0.
-      set (p := fun x : w => val2 x =? 0).
-      assert (first_nat p witness1 <= first_nat p witness2).
-      { apply well_ordering_principle...
-        unfold p...
-        apply well_ordering_principle...
-        unfold p...
-      }
-      assert (first_nat p witness2 <= first_nat p witness1).
-      { apply well_ordering_principle...
-        unfold p...
-        apply well_ordering_principle...
-        unfold p...
-      }
-      lia.
-    - simpl... intros. apply IHn... simpl...
-  }
-  induction e.
-  - intros.
-    inversion H.
-    inversion H0.
-    subst.
-    assert (n2 = n1) by lia.
-    subst.
-    assert (varEval n1 n = val1) by Arity_eq.
-    assert (varEval n1 n = val2) by Arity_eq.
-    subst.
-    apply extensionality_refl.
-  - intros.
-    inversion H.
-    inversion H0.
-    subst.
-    assert (plusEval n val0 val3 = val1) by Arity_eq.
-    assert (plusEval n val4 val5 = val2) by Arity_eq.
-    subst.
-    apply extensionality_lift2...
-  - intros.
-    inversion H.
-    inversion H0.
-    subst.
-    assert (multEval n val0 val3 = val1) by Arity_eq.
-    assert (multEval n val4 val5 = val2) by Arity_eq.
-    subst.
-    apply extensionality_lift2...
-  - intros.
-    inversion H.
-    inversion H0.
-    subst.
-    assert (ltEval n val0 val3 = val1) by Arity_eq.
-    assert (ltEval n val4 val5 = val2) by Arity_eq.
-    subst.
-    apply extensionality_lift2...
-  - intros.
-    inversion H.
-    inversion H0.
-    subst.
-    assert (muEval n val0 witness = val1) by Arity_eq.
-    assert (muEval n val3 witness0 = val2) by Arity_eq.
-    subst...
-Qed.
-
-Definition correpondsToFunc (n : nat) : Arity n w -> Arity n w -> Prop :=
-  fun val1 : Arity n w => fun func : Arity n w => universal n (liftArity2 n (fun x1 : w => fun x : w => x1 = x) val1 func)
-.
-
-Definition correpondsToRel (n : nat) : Arity n w -> Arity n Prop -> Prop :=
-  fun val1 : Arity n w => fun rel : Arity n Prop => universal n (liftArity2 n (fun x1 : w => fun p : Prop => if Nat.eq_dec x1 0 then p else ~ p) val1 rel)
-.
-
-Definition isBoolean (n : nat) : Arity n w -> Prop :=
-  fun val1 : Arity n w => universal n (liftArity1 n (fun x1 : w => x1 = 0 \/ x1 = 1) val1)
-.
-
-Inductive IsArith : forall n : nat, Arity n Prop -> Prop :=
-| IsArithmetic :
-  forall n : nat,
-  forall e : Arith,
-  forall val : Arity n w,
-  forall rel : Arity n Prop,
-  RuleArith n e val ->
-  isBoolean n val ->
-  correpondsToRel n val rel ->
-  IsArith n rel
-.
-
-Definition has_characterstic_fun_then_Arith_dec (n : nat) :
-  forall val : Arity n w,
-  forall P : Arity n Prop,
-  correpondsToRel n val P ->
-  Arity_dec n P (liftArity1 n (fun p : Prop => ~ p) P).
-Proof with eauto.
-  unfold correpondsToRel.
-  unfold liftArity2.
-  unfold liftArity1.
-  induction n.
-  - simpl.
-    intros.
-    destruct (Nat.eq_dec val 0).
-    + left.
-      exact H.
-    + right.
-      exact H.
-  - simpl.
-    intros.
-    apply (IHn (val m) (P m) (H m)).
-Qed.
-
-Lemma ltEval_correpondsTo_less_than :
-  forall n : nat,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  correpondsToRel n (ltEval n val1 val2) (liftArity2 n (fun x1 : w => fun x2 : w => x1 < x2) val1 val2).
-Proof with eauto.
-  unfold correpondsToRel.
-  induction n.
-  - unfold liftArity2.
-    simpl.
-    unfold ltEval.
-    simpl.
-    intros.
-    destruct (Compare_dec.lt_dec val1 val2); simpl; firstorder.
-  - simpl...
-Qed.
-
-Fixpoint numEval (m : nat) : Arity 0 w :=
-  match m with
-  | 0 => muEval 0 (varEval 0 0) 0
-  | S m' => muEval 0 (ltEval 1 (pureArity 1 (numEval m')) (varEval 0 0)) (S m')
-  end
-.
-
-Lemma numEval_correpondsTo_numeral :
+  evalArith (S n) (projA n) (proj n)
+| plusE :
+  evalArith 2 plusA plus
+| multE :
+  evalArith 2 multA mult
+| lessE :
+  evalArith 2 lessA less
+| liftE :
   forall m : nat,
-  correpondsToFunc 0 (numEval m) m.
-Proof with eauto.
-  assert ( claim1 :
-    forall x : nat,
-    first_nat (fun x1 : w => Nat.eqb (if Compare_dec.lt_dec x x1 then 0 else 1) 0) (S x) = S x
-  ).
-  { intros x.
-    set (p := fun x1 : w => (if Compare_dec.lt_dec x x1 then 0 else 1) =? 0).
-    assert (p (S x) = true).
-    { unfold p.
-      destruct (Compare_dec.lt_dec x (S x)).
-      - tauto.
-      - lia.
-    }
-    assert (first_nat p (S x) <= S x).
-    { apply well_ordering_principle...
-    }
-    assert (first_nat p (S x) > x).
-    { unfold p.
-      simpl.
-      fold p.
-      destruct (Compare_dec.lt_dec x (first_nat p x)).
-      - simpl; lia.
-      - simpl; lia.
-    }
-    lia.
-  }
-  unfold correpondsToFunc.
-  induction m.
-  - reflexivity.
-  - simpl...
-    assert (numEval m = m) by apply IHm.
-    rewrite H.
-    apply claim1...
-Qed.
-
-Fixpoint numArith (m : nat) : Arith :=
-  match m with
-  | 0 => muArith (varArith 0)
-  | S m' => muArith (ltArith (numArith m') (varArith 0))
-  end
-.
-
-Lemma numRule :
-  forall m : nat,
-  RuleArith 0 (numArith m) (numEval m).
-Proof with eauto.
-  induction m.
-  - execRule.
-  - execRule.
-    unfold ltEval.
-    unfold varEval.
-    simpl.
-    rewrite numEval_correpondsTo_numeral.
-    destruct (Compare_dec.lt_dec m (S m)); lia.
-Qed.
-
-Definition notEval (n : nat) : Arity n w -> Arity n w :=
-  fun val1 : Arity n w => ltEval n (pureArity n (numEval 0)) val1
-.
-
-Lemma notEval_isBoolean :
   forall n : nat,
+  forall e1 : arith,
   forall val1 : Arity n w,
-  isBoolean n (notEval n val1).
-Proof with eauto.
-  unfold isBoolean.
-  unfold notEval.
-  unfold liftArity1.
-  induction n.
-  - simpl.
-    unfold ltEval.
-    unfold muEval.
-    unfold varEval.
-    simpl.
-    intros.
-    destruct (Compare_dec.lt_dec 0 val1); firstorder.
-  - simpl...
-Qed.
-
-Lemma notEval_correpondsTo_negation :
+  evalArith n e1 val1 ->
+  evalArith (m + n) (liftA e1) (lift m n val1)
+| callE :
   forall n : nat,
-  forall val1 : Arity n w,
-  forall P : Arity n Prop,
-  correpondsToRel n val1 P ->
-  correpondsToRel n (notEval n val1) (liftArity1 n (fun p : Prop => ~ p) P).
-Proof with eauto.
-  unfold correpondsToRel.
-  induction n.
-  - unfold liftArity2.
-    unfold notEval.
-    simpl.
-    fold (numEval 0).
-    rewrite numEval_correpondsTo_numeral.
-    unfold liftArity1.
-    simpl.
-    intros.
-    assert (if Nat.eq_dec (ltEval 0 0 val1) 0 then 0 < val1 else ~ 0 < val1) by apply (ltEval_correpondsTo_less_than 0 0 val1).
-    destruct (Nat.eq_dec val1 0).
-    + subst.
-      simpl.
-      tauto.
-    + unfold ltEval in *.
-      simpl in *.
-      destruct (Compare_dec.lt_dec 0 val1); lia || tauto.
-  - simpl...
-Qed.
-
-Definition notArith (e1 : Arith) : Arith :=
-  ltArith (numArith 0) e1
-.
-
-Lemma notRule :
-  forall n : nat,
-  forall e1 : Arith,
-  forall val1 : Arity n w,
-  RuleArith n e1 val1 ->
-  RuleArith n (notArith e1) (notEval n val1).
-Proof with eauto.
-  unfold notArith.
-  unfold notEval.
-  intros.
-  execRule.
-Qed.
-
-Definition orEval (n : nat) : Arity n w -> Arity n w -> Arity n w :=
-  fun val1 : Arity n w => fun val2 : Arity n w => notEval n (notEval n (multEval n val1 val2))
-.
-
-Lemma orEval_isBoolean :
-  forall n : nat,
-  forall val1 : Arity n w,
+  forall e1 : arith,
+  forall e2 : arith,
+  forall val1 : Arity (S n) w,
   forall val2 : Arity n w,
-  isBoolean n (orEval n val1 val2).
-Proof with eauto.
-  unfold orEval.
-  unfold isBoolean.
-  unfold liftArity1.
-  induction n.
-  - simpl.
-    unfold multEval.
-    simpl.
-    lia.
-  - simpl...
-Qed.
-
-Lemma orEval_correpondsTo_disjunction :
+  evalArith (S n) e1 val1 ->
+  evalArith n e2 val2 ->
+  evalArith n (callA e1 e2) (call n val1 val2)
+| miniE :
   forall n : nat,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  forall P1 : Arity n Prop,
-  forall P2 : Arity n Prop,
-  correpondsToRel n val1 P1 ->
-  correpondsToRel n val2 P2 ->
-  correpondsToRel n (orEval n val1 val2) (liftArity2 n (fun p1 : Prop => fun p2 : Prop => p1 \/ p2) P1 P2).
-Proof with eauto.
-  unfold correpondsToRel.
-  unfold orEval.
-  induction n.
-  - simpl.
-    unfold liftArity2.
-    simpl.
-    intros.
-    destruct (Nat.eq_dec val1 0); destruct (Nat.eq_dec val2 0).
-    + subst; simpl; tauto.
-    + subst; simpl; tauto.
-    + unfold multEval; simpl.
-      assert (val1 * val2 = 0) by lia.
-      rewrite H1.
-      simpl; tauto.
-    + unfold multEval; simpl.
-      destruct (Nat.eq_dec (val1 * val2) 0); lia || tauto.
-  - simpl...
-Qed.
-
-Definition orArith (e1 : Arith) (e2 : Arith) : Arith :=
-  multArith e1 e2
-.
-
-Lemma orRule :
-  forall n : nat,
-  forall e1 : Arith,
-  forall e2 : Arith,
-  forall val1 : Arity n w,
-  forall val2 : Arity n w,
-  RuleArith n e1 val1 ->
-  RuleArith n e2 val2 ->
-  RuleArith n (orArith e1 e2) (orEval n val1 val2).
-Proof with eauto.
-  unfold orArith; unfold orEval.
-  intros.
-  execRule.
-Qed.
-
-Definition andEval (n : nat) : Arity n w -> Arity n w -> Arity n w :=
-  fun val1 : Arity n w => fun val2 : Arity n w => notEval n (orEval n (notEval n val1) (notEval n val2))
+  forall e1 : arith,
+  forall val1 : Arity (S n) w,
+  forall witness : Arity n w,
+  evalArith (S n) e1 val1 ->
+  universal n (liftArity1 n (fun x : w => x = 0) (call n val1 witness)) ->
+  evalArith n (miniA e1) (mini n val1 witness) 
 .
 
 End Arithmetic.
